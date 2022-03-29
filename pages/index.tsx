5fd8f97eb2486b53bd6cca842fc97f11b5dddfc2
Carlos <carlos.ramirez@poincenot.com>
2022-03-29 10:25:17 -0300

    🚀 deployy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Catalog from '../components/Catalog'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
 import NavBar from '../components/Navbar'
 import Walkthrough from '../components/Walkthrough'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
@@ -19,7 +17,7 @@ const Home: NextPage = () => {
         />
         <meta property='og:title' content='Desafio AeroLab - Carlos Ramirez' />
         <meta property='og:locale' content='es_AR' />
-        <meta property='og:url' content='google.com' />
+        <meta property='og:url' content='https://aerochallengex.vercel.app/' />
         <meta property='og:type' content='website' />
         <meta
           property='og:description'
@@ -28,7 +26,7 @@ const Home: NextPage = () => {
         <meta property='og:image' content='https://i.imgur.com/afIg9Zl.png' />
         <meta name='twitter:card' content='summary' />
         <meta name='twitter:title' content='Desafio Aerolab - Carlos Ramirez' />
-        <meta name='twitter:url' content='google.com' />
+        <meta name='twitter:url' content='https://aerochallengex.vercel.app/' />
         <meta
           name='twitter:description'
           content='Desafio Aerolab front-end hecho por Carlos Ramirez'
@@ -36,7 +34,7 @@ const Home: NextPage = () => {
         <meta name='twitter:image' content='https://i.imgur.com/afIg9Zl.png' />
         <meta
           name='twitter:image:alt'
-          content='Desafio AerloLab - Carlos Ramirez'
+          content='Desafio AeroLab - Carlos Ramirez'
         />
         <title>Aerolab Challenge</title>
       </Head>

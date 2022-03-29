bea7b7dfb98d058c1155197a7ece7bb99e3629e1
Carlos <carlos.ramirez@poincenot.com>
2022-03-28 22:28:49 -0300

    feat: catalog

---
+++ components/Catalog/index.tsx
@@ -0,0 +1,240 @@
+import { ChangeEvent, useEffect, useState } from 'react'
+import Image from 'next/image'
+import NotificationsContainer from '../NotificationsContainer'
+import Pagination from '../Pagination'
+import Product from '../Product'
+import { StyledCatalog, MobilePagination } from './styles'
+
+export interface Product {
+  _id: string
+  name: string
+  cost: number
+  quantity: number
+  category: string
+  img: {
+    url: string
+    hdUrl: string
+  }
+}
+
+type SortTypes = 'Most Recent' | 'Lowest Price' | 'Highest Price'
+
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([])
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [defaultSort, setDefaultSort] = useState<SortTypes>('Most Recent')
+  const [mostRecentProduct, setMostRecentProduct] = useState<Product[]>([])
+  const [amountPerPage, setAmountPerPage] = useState(16)
+  const [filtered, setFiltered] = useState(false)
+  const categorias = Array.from(new Set(products.map(p => p.category)))
+
+  const resizeFunction = () => {
+    if (window.innerWidth <= 375) {
+      setAmountPerPage(8)
+    } else if (window.innerWidth <= 1024) {
+      setAmountPerPage(12)
+    } else {
+      setAmountPerPage(16)
+    }
+  }
+
+  useEffect(() => {
+    setFilteredProducts(products => products.slice(0, amountPerPage))
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    window.addEventListener('resize', resizeFunction)
+    return () => {
+      window.removeEventListener('resize', resizeFunction)
+    }
+  }, [])
+
+  useEffect(() => {
+    fetch('https://coding-challenge-api.aerolab.co/products', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQxZTAyNGRlNjZjMTAwMjFmMTVkNzkiLCJpYXQiOjE2NDg0ODQzODh9.cVu7GHdtSVR2yD935rDw1uhcNrywsq0sbI3hG-Ql-4E'
+      }
+    })
+      .then(response => response.json())
+      .then(response => {
+        setMostRecentProduct(response)
+        setProducts(response)
+        resizeFunction()
+        setFilteredProducts(response.slice(0, amountPerPage))
+      })
+  }, []) //eslint-disable-line react-hooks/exhaustive-deps
+
+  const displayProducts = () => {
+    if (currentPage === 1) {
+      setFilteredProducts(() => products.slice(0, amountPerPage))
+    } else {
+      setFilteredProducts(() =>
+        products.slice(
+          amountPerPage * (currentPage - 1),
+          amountPerPage * currentPage
+        )
+      )
+    }
+  }
+
+  useEffect(() => displayProducts(), [currentPage]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  const handleSort = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === 'Most Recent') {
+      setCurrentPage(1)
+      setProducts(mostRecentProduct)
+      setFilteredProducts(mostRecentProduct.slice(0, amountPerPage))
+      setDefaultSort('Most Recent')
+      return
+    }
+    setCurrentPage(1)
+    let productsCopy = [...products]
+    setProducts(
+      productsCopy.sort((a, b) => {
+        const costA = a.cost
+        const costB = b.cost
+        return e.target.value === 'Lowest Price' ? costA - costB : costB - costA
+      })
+    )
+    setFilteredProducts(productsCopy.slice(0, amountPerPage))
+    setDefaultSort(
+      e.target.value === 'Lowest Price' ? 'Lowest Price' : 'Highest Price'
+    )
+  }
+
+  const handleFilter = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === 'all') {
+      setFilteredProducts(products.slice(0, amountPerPage))
+      setFiltered(false)
+      setCurrentPage(1)
+      return
+    }
+    setFilteredProducts(products.filter(p => p.category === e.target.value))
+    setFiltered(true)
+    setCurrentPage(1)
+  }
+
+  const handleMobilePaginate = () => {
+    const mobileCurrentProducts = filteredProducts.length + amountPerPage
+    setFilteredProducts(products.slice(0, mobileCurrentProducts))
+  }
+
+  return (
+    <StyledCatalog id='catalog'>
+      <NotificationsContainer />
+      <h2>
+        <span>TECH</span> PRODUCTS
+      </h2>
+      <div className='container'>
+        <div className='flex__container'>
+          <div className='filter__container'>
+            <label
+              className={`label__filter ${
+                amountPerPage !== 16 ? 'temp__fix' : ''
+              }`}
+              htmlFor='filter'
+            >
+              Filter By
+            </label>
+            <select
+              onChange={handleFilter}
+              className='filter__select'
+              id='filter'
+            >
+              <option value='all'>All Products</option>
+              {categorias.map(c => {
+                return (
+                  <option key={c} value={c}>
+                    {c}
+                  </option>
+                )
+              })}
+            </select>
+          </div>
+          <div className='sort__container'>
+            <p className={amountPerPage !== 16 ? 'temp__fix' : ''}>Sort by:</p>
+            <input
+              type='radio'
+              name='points'
+              id='value1'
+              value='Most Recent'
+              checked={defaultSort === 'Most Recent'}
+              onChange={handleSort}
+            />
+            <label htmlFor='value1'>
+              <span>Most Recent</span>
+            </label>
+            <input
+              type='radio'
+              name='points'
+              id='value2'
+              value='Lowest Price'
+              checked={defaultSort === 'Lowest Price'}
+              onChange={handleSort}
+            />
+            <label htmlFor='value2'>
+              <span>Lowest Price</span>
+            </label>
+            <input
+              type='radio'
+              name='points'
+              id='value3'
+              value='Highest Price'
+              checked={defaultSort === 'Highest Price'}
+              onChange={handleSort}
+            />
+            <label htmlFor='value3'>
+              <span>Highest Price</span>
+            </label>
+          </div>
+        </div>
+        {amountPerPage !== 8 && (
+          <Pagination
+            productAmount={filtered ? filteredProducts.length : products.length}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            amountPerPage={amountPerPage}
+          />
+        )}
+      </div>
+      <div className='products'>
+        {filteredProducts.map(p => (
+          <Product product={p} key={p._id} />
+        ))}
+      </div>
+      <footer>
+        <p>
+          <span className='span'>
+            {filteredProducts.length} of {products.length}
+          </span>
+          products
+        </p>
+      </footer>
+      {amountPerPage > 8 ? (
+        <Pagination
+          productAmount={filtered ? filteredProducts.length : products.length}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          amountPerPage={amountPerPage}
+        />
+      ) : (
+        <MobilePagination
+          onClick={handleMobilePaginate}
+          disabled={filteredProducts.length == products.length}
+        >
+          <Image
+            src='/assets/icons/aeropay-1.svg'
+            width='32'
+            height='32'
+            alt='aeroLogo'
+          />
+          <span>Show More</span>
+        </MobilePagination>
+      )}
+    </StyledCatalog>
+  )
+}
---
+++ components/Catalog/styles.ts
@@ -0,0 +1,233 @@
+import styled from 'styled-components'
+
+export const StyledCatalog = styled.section`
+  max-width: var(--max-width);
+  width: calc(100vw - 40px);
+  margin: 200px auto 0;
+
+  h2 {
+    font-size: var(--ff-heading-200);
+    font-weight: 900;
+    margin-bottom: 42px;
+    span {
+      color: var(--blue-1);
+    }
+  }
+
+  .container {
+    display: flex;
+    font-size: var(--ff-body-200);
+    font-weight: 600;
+    align-items: center;
+    color: var(--grey-600);
+  }
+
+  .flex__container {
+    display: flex;
+  }
+
+  .filter__container {
+    margin-right: 40px;
+    padding-right: 40px;
+    border-right: 2px solid var(--grey-300);
+  }
+
+  .label__filter {
+    margin-right: 16px;
+  }
+
+  .filter__select {
+    padding: 16px 24px;
+    border-radius: 16px;
+    border: 1px solid #dae4f2;
+    color: var(--grey-600);
+    background-color: white;
+    font-size: inherit;
+    transition: transform 300ms;
+
+    &:hover,
+    &:focus {
+      transform: translateY(-3px) scale(1.03);
+      outline: none;
+      border-color: #f279f2;
+    }
+
+    &:active {
+      border-color: #f279f2;
+    }
+  }
+
+  .sort__container {
+    display: flex;
+    align-items: center;
+
+    p {
+      margin-right: 40px;
+    }
+
+    input {
+      appearance: none;
+    }
+    label {
+      background-color: var(--brand-light);
+      padding: 8px 24px;
+      border-radius: 12px;
+      font-size: 18px;
+      margin-right: 12px;
+      cursor: pointer;
+      transition: transform 300ms;
+
+      &:hover {
+        transform: scale(1.05);
+      }
+    }
+    span {
+      background-image: var(--brand-gradient);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+    }
+    input:checked {
+      & + label {
+        background-image: var(--brand-gradient);
+        span {
+          color: white;
+          background: none;
+        }
+      }
+    }
+  }
+
+  .products {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 80px 24px;
+    margin-top: 67px;
+  }
+
+  footer {
+    margin-top: 64px;
+    text-align: center;
+    position: relative;
+    padding: 20px 0;
+
+    p {
+      color: var(--grey-600);
+    }
+    .span {
+      color: var(--blue-1);
+      margin-right: 5px;
+    }
+
+    & > div:last-child {
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
+  }
+
+  @media (max-width: 1024px) {
+    padding: 0 20px;
+
+    h2 {
+      font-size: 32px;
+    }
+
+    .flex__container {
+      flex-direction: column;
+    }
+
+    .container {
+      align-items: flex-start;
+    }
+
+    .filter__container {
+      padding: 0;
+      border: none;
+      margin: 0 0 27px;
+    }
+
+    .products {
+      justify-content: center;
+      gap: 48px 24px;
+    }
+
+    footer {
+      display: flex;
+      flex-direction: column-reverse;
+      justify-content: center;
+      align-items: center;
+
+      & > div:last-child {
+        position: static;
+        margin-left: 0;
+        margin-bottom: 20px;
+      }
+    }
+  }
+
+  @media (max-width: 730px) {
+    h2 {
+      text-align: center;
+    }
+    .filter__select,
+    .filter__container {
+      width: calc(100% - 20px);
+    }
+
+    .container {
+      & > div:last-child {
+        display: none;
+      }
+    }
+
+    .sort__container {
+      width: calc(100vw - 40px);
+      overflow-x: auto;
+      display: block;
+      white-space: nowrap;
+
+      label {
+        display: inline-block;
+        padding: 8px 16px;
+        font-size: 16px;
+        margin-right: 4px;
+      }
+    }
+  }
+`
+
+export const MobilePagination = styled.button`
+  position: relative;
+  margin: 0 auto;
+  border: 1px solid var(--grey-300);
+  padding: 8px 16px;
+  border-radius: 16px;
+  background-color: var(--white);
+  display: flex;
+  align-items: center;
+  font-size: var(--ff-body-100);
+  width: 156px;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.05);
+  cursor: pointer;
+  transition: transform 300ms;
+  color: var(--grey-900);
+
+  span {
+    margin: 0 auto;
+  }
+
+  &:hover,
+  &:focus {
+    transform: translateY(-3px) scale(1.03);
+    outline: none;
+    border-color: #f279f2;
+  }
+  &:disabled {
+    background-image: none;
+    background-color: var(--grey-200);
+    color: var(--grey-600);
+    pointer-events: none;
+  }
+`
